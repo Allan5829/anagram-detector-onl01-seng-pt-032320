62f32c4dcadc5d7875c689a01b133bce6e0ffbbc
Allan Farca <allan.farca@gmail.com>
2020-04-18 04:58:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,16 @@ class Anagram
   
   def match (array_of_words)
     base_word_letters = []
+    anagram_results = []
+    
     base_word_letters << @base_word.split("")
-    possible_anagrams = array_of_words
+    possible_anagrams = array_of_words.split(" ")
+    
+    possible_anagrams.each do |words|
+      possible_anagrams_letters = []
+      possible_anagrams_letters << words.split("")
+      end 
+    end 
   end 
   
   
