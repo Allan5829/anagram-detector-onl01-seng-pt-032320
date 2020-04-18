63caf62a7f2bea7b6379c81a407c036e5ffe34a9
Allan Farca <allan.farca@gmail.com>
2020-04-18 05:07:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Anagram
   attr_accessor :base_word
   
@@ -11,7 +12,7 @@ class Anagram
     
     base_word_letters << @base_word.split("")
     base_word_letters.flatten.sort
-    
+    binding.pry
     array_of_words.each do |words|
       possible_anagrams_letters = []
       possible_anagrams_letters << words.split("")
