f50bd39ec6aeaf81e6734f9e8e4dfd1c82e2b073
Allan Farca <allan.farca@gmail.com>
2020-04-18 04:50:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,9 @@ class Anagram
   end 
   
   def match (array_of_words)
+    base_word_letters = []
+    base_word_letters << @base_word.split("")
+    possible_anagrams = array_of_words
   end 
   
   
