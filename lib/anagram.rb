abe32e8c57c8a8b362097bdffe167e7bebf0043f
Allan Farca <allan.farca@gmail.com>
2020-04-18 04:47:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :base_word
+  
+  def initialize (base_word)
+    @base_word = base_word
+  end 
+  
+  def match (array_of_words)
+  end 
+  
+  
+end 
